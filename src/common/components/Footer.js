07b7ded41294358e47bad801c88cb88203280550
RishabhBudhia <RishabhBudhia@users.noreply.github.com>
2021-11-03 11:31:39 +0530

    made landing page

---
+++ src/common/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../../../styles/Landing.module.css";
+
+const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      <span className={styles.footer__text}>Made with coffee and Love</span>
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,29 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import LandingPage from '../common/components/Landing'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import LandingPage from "../common/components/Landing";
+import Camera from "../common/components/Camera";
+import { useState } from "react";
 
 export default function Home() {
+  const [state, setState] = useState(false);
+
+  const changeState = () => {
+    console.log(state);
+    setState(!state);
+  };
+
   return (
     <>
       <Head>
         <title>Sense</title>
-        <meta name="description" content="An app that can give outfit recommendations to complete your wardrobe." />
+        <meta
+          name="description"
+          content="An app that can give outfit recommendations to complete your wardrobe."
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <LandingPage /> 
-      </>
-  )
+      {state ? <Camera /> : <LandingPage onClick={changeState} />}
+    </>
+  );
 }
-
