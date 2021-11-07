d2df7012d32d984612eec67a4867a444f26b6086
RishabhBudhia <RishabhBudhia@users.noreply.github.com>
2021-11-07 16:55:42 +0530

    added icons in footer

--- src/common/components/Footer.js
+++ src/common/components/Footer.js
@@ -1,10 +1,15 @@
 import React from "react";
 import styles from "../../../styles/Landing.module.css";
+import { FaCoffee } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      <span className={styles.footer__text}>Made with coffee and Love</span>
+      <span className={styles.footer__text}>
+        Made with <FaCoffee className={styles.icon_style} />
+        and <FaHeart className={styles.icon_style} />
+      </span>
     </div>
   );
 };
